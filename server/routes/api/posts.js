5ae31aa5b95dcdedebe81f45bbf66b9428c67c83
Devin Herath <devdhera@gmail.com>
2018-10-08 23:26:14 +0530

    Added Posts Route

---
+++ server/routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+
+module.exports = router;
